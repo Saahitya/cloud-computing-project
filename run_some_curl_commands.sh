b16328e492744800954ca27827f4178277e1f1fb
ShashankBongale <shashankbongale@gmail.com>
2019-02-12 23:36:33 +0530

    upvote done

--- run_some_curl_commands.sh
+++ run_some_curl_commands.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "curl -i -H "Content-Type: application/json" -X POST -d '["category3"]' http://localhost:5000/api/category/add"
-curl -i -H "Content-Type: application/json" -X POST -d '["category3"]' http://localhost:5000/api/category/add
+echo "curl -i -H "Content-Type: application/json" -X POST -d '["category3"]' http://localhost:5000/api/v1/category/"
+curl -i -H "Content-Type: application/json" -X POST -d '["category100"]' http://localhost:5000/api/v1/categories
 
-echo "curl -i -H "Content-Type: application/json" -X GET -d '{}' http://localhost:5000/api/category/list"
+echo "curl -i -H "Content-Type: application/json" -X GET -d '{}' http://localhost:5000/api/v1/categories"
 curl -i -H "Content-Type: application/json" -X GET -d '{}' http://localhost:5000/api/v1/categories
 
 echo "curl -i -H "Content-Type: application/json" -X DELETE -d '["category2"]' http://localhost:5000/api/category/remove"
