0f16f366183814964f6f7907bc6eb75e01f1ad07
Ubuntu <ubuntu@ip-172-31-86-224.ec2.internal>
2019-04-17 18:56:45 +0000

    HTTP forwarding done

--- orchas.py
+++ orchas.py
@@ -1,3 +1,163 @@
-import os
-l = os.system("sudo docker")
-print(l)
+from flask import Flask, jsonify, request, abort,redirect, url_for , Response
+import requests
+import re
+import pickle
+app = Flask(__name__)
+@app.route('/api/v1/categories', methods=['GET'])
+def fun():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/categories', methods=['POST'])
+def cat_post():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/categories/<string:categoryName>', methods=['DELETE'])
+def rem_cat(categoryName):
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/categories/<string:categoryName>/acts', methods=['GET'])
+def list_acts_for_category(categoryName):
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/categories/<string:categoryName>/acts/size', methods=['GET'])
+def number_of_acts_for_category(categoryName):
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/acts/upvote', methods=['POST'])
+def upvote_an_act():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/acts/<int:task_id>',methods = ['DELETE'])
+def delete_act(task_id):
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/acts', methods=['POST'])
+def upload_an_act():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/_count',methods=['GET'])
+def count_fun():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/_count',methods=['DELETE'])
+def del_count():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+@app.route('/api/v1/acts/count',methods=['GET'])
+def count1():
+    old_url = request.url
+    parts = old_url.split("http://35.171.62.224")
+    new_url = "http://127.0.0.1:8000"+parts[1]
+    resp = requests.request(
+        method=request.method,
+        url= new_url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data())
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0",port=80)
+
