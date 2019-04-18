4b7343885cbaa590e1a5d2660f5d10d4dc64505e
ShashankBongale <shashankbongale@gmail.com>
2019-04-18 11:56:44 +0530

    init container

--- orchas.py
+++ orchas.py
@@ -2,7 +2,16 @@ from flask import Flask, jsonify, request, abort,redirect, url_for , Response
 import requests
 import re
 import pickle
+import time
+import threading
+import os
+no_of_connections = 0
 app = Flask(__name__)
+cont_dict = {}
+def init_container():
+    con = os.popen("sudo docker run -p 8000:80 -d acts").read()
+    con_real = con.rstrip()
+    con_dict[8000] = con_real
 @app.route('/api/v1/categories', methods=['GET'])
 def fun():
     old_url = request.url
@@ -80,7 +89,7 @@ def upvote_an_act():
     new_url = "http://127.0.0.1:8000"+parts[1]
     resp = requests.request(
         method=request.method,
-        url= new_url,
+        url= new_url,    t1 = threading.Thread(target=print_square, args=(10,))
         headers={key: value for (key, value) in request.headers if key != 'Host'},
         data=request.get_data())
     headers = [(name, value) for (name, value) in resp.raw.headers.items()]
@@ -159,5 +168,8 @@ def count1():
 
 
 if __name__ == '__main__':
+    init_container()
     app.run("0.0.0.0",port=80)
-
+    t1 = threading.Thread(target=auto_scale)
+    t1.start()
+    t1.join()
